e23fd15c7525b5605e667684305eb7f5578fbc57
isaotoyama <86751458+isaotoyama@users.noreply.github.com>
2023-10-04 05:59:17 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,128 @@ import Stats from "three/examples/jsm/libs/stats.module";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { GUI } from "dat.gui";
 // import Stats from "three/examples/jsm/libs/stats.module";
-// import gsap from "gsap";
-// import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
 // clearing the console (just a CodePen thing)
 
 console.clear();
 
+const scene = new THREE.Scene();
+scene.add(new THREE.AxesHelper(5));
+
+const light1 = new THREE.PointLight(0xffffff, 100);
+light1.position.set(2.5, 2.5, 2.5);
+scene.add(light1);
+
+const light2 = new THREE.PointLight(0xffffff, 100);
+light2.position.set(-2.5, 2.5, 2.5);
+scene.add(light2);
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(0.8, 1, 1.0);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.target.set(0, 1, 0);
+
+let mixer: THREE.AnimationMixer;
+let modelReady = false;
+const animationActions: THREE.AnimationAction[] = [];
+let activeAction: THREE.AnimationAction;
+let lastAction: THREE.AnimationAction;
+const gltfLoader = new GLTFLoader();
+
+gltfLoader.load(
+  "./src/plane/scene.gltf",
+  (gltf) => {
+    // gltf.scene.scale.set(.01, .01, .01)
+
+    mixer = new THREE.AnimationMixer(gltf.scene);
+
+    const animationAction = mixer.clipAction((gltf as any).animations[0]);
+    animationActions.push(animationAction);
+    animationsFolder.add(animations, "default");
+    activeAction = animationActions[0];
+    modelReady = true;
+    (gltf as any).animations[0].tracks.shift(); //delete the specific track that moves the object forward while running
+
+    scene.add(gltf.scene);
+
+    //   //add an animation from another file
+    //   gltfLoader.load(
+    //     "./src/plane/scene.gltf",
+    //     (gltf) => {
+    //       console.log("loaded 1");
+    //       const animationAction = mixer.clipAction((gltf as any).animations[0]);
+    //       animationActions.push(animationAction);
+    //       animationsFolder.add(animations, "1");
+
+    //       //add an animation from another file
+    //       // gltfLoader.load(
+    //       //   "./src/plane/scene.gltf",
+    //       //   (gltf) => {
+    //       //     console.log("loaded 2");
+    //       //     const animationAction = mixer.clipAction(
+    //       //       (gltf as any).animations[0]
+    //       //     );
+    //       //     animationActions.push(animationAction);
+    //       //     animationsFolder.add(animations, "2");
+
+    //       //     //add an animation from another file
+    //       //     gltfLoader.load(
+    //       //       "./src/plane/scene.gltf",
+    //       //       (gltf) => {
+    //       //         console.log("loaded 3");
+    //       //         (gltf as any).animations[0].tracks.shift(); //delete the specific track that moves the object forward while running
+    //       //         const animationAction = mixer.clipAction(
+    //       //           (gltf as any).animations[0]
+    //       //         );
+    //       //         animationActions.push(animationAction);
+    //       //         animationsFolder.add(animations, "3");
+
+    //       //         modelReady = true;
+    //       //       },
+    //       //       (xhr) => {
+    //       //         console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+    //       //       },
+    //       //       (error) => {
+    //       //         console.log(error);
+    //       //       }
+    //       //     );
+    //       //   },
+    //       //   (xhr) => {
+    //       //     console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+    //       //   },
+    //       //   (error) => {
+    //       //     console.log(error);
+    //       //   }
+    //       // );
+    //     },
+    //     (xhr) => {
+    //       console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+    //     },
+    //     (error) => {
+    //       console.log(error);
+    //     }
+    //   );
+  },
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  (error) => {
+    console.log(error);
+  }
+);
 // there are 3 parts to this
 //
 // Scene:           Setups and manages threejs rendering
@@ -199,281 +314,254 @@ console.clear();
 //   // );
 // }
 
-// function setupAnimation(model) {
-//   let scene = new Scene(model);
-//   let plane = scene.modelGroup;
+function setupAnimation(model: any) {
+  let scene = new THREE.Scene(model);
+  let plane = scene.modelGroup;
 
-//   gsap.fromTo(
-//     "canvas",
-//     { x: "50%", autoAlpha: 0 },
-//     { duration: 1, x: "0%", autoAlpha: 1 }
-//   );
-//   gsap.to(".loading", { autoAlpha: 0 });
-//   gsap.to(".scroll-cta", { opacity: 1 });
-//   gsap.set("svg", { autoAlpha: 1 });
+  gsap.fromTo(
+    "canvas",
+    { x: "50%", autoAlpha: 0 },
+    { duration: 1, x: "0%", autoAlpha: 1 }
+  );
+  gsap.to(".loading", { autoAlpha: 0 });
+  gsap.to(".scroll-cta", { opacity: 1 });
+  gsap.set("svg", { autoAlpha: 1 });
 
-//   let tau = Math.PI * 2;
+  let tau = Math.PI * 2;
 
-//   gsap.set(plane.rotation, { y: tau * -0.25 });
-//   gsap.set(plane.position, { x: 80, y: -32, z: -60 });
+  gsap.set(plane.rotation, { y: tau * -0.25 });
+  gsap.set(plane.position, { x: 80, y: -32, z: -60 });
 
-//   scene.renderer();
+  scene.renderer();
 
-//   let sectionDuration = 1;
-//   gsap.fromTo(
-//     scene.views[1],
-//     { height: 1, bottom: 0 },
-//     {
-//       height: 0,
-//       bottom: 1,
-//       ease: "none",
-//       scrollTrigger: {
-//         trigger: ".blueprint",
-//         scrub: true,
-//         start: "bottom bottom",
-//         end: "bottom top",
-//       },
-//     }
-//   );
+  let sectionDuration = 1;
+  gsap.fromTo(
+    scene.views[1],
+    { height: 1, bottom: 0 },
+    {
+      height: 0,
+      bottom: 1,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".blueprint",
+        scrub: true,
+        start: "bottom bottom",
+        end: "bottom top",
+      },
+    }
+  );
 
-//   gsap.fromTo(
-//     scene.views[1],
-//     { height: 0, bottom: 0 },
-//     {
-//       height: 1,
-//       bottom: 0,
-//       ease: "none",
-//       scrollTrigger: {
-//         trigger: ".blueprint",
-//         scrub: true,
-//         start: "top bottom",
-//         end: "top top",
-//       },
-//     }
-//   );
+  gsap.fromTo(
+    scene.views[1],
+    { height: 0, bottom: 0 },
+    {
+      height: 1,
+      bottom: 0,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".blueprint",
+        scrub: true,
+        start: "top bottom",
+        end: "top top",
+      },
+    }
+  );
 
-//   gsap.to(".ground", {
-//     y: "30%",
-//     scrollTrigger: {
-//       trigger: ".ground-container",
-//       scrub: true,
-//       start: "top bottom",
-//       end: "bottom top",
-//     },
-//   });
+  gsap.to(".ground", {
+    y: "30%",
+    scrollTrigger: {
+      trigger: ".ground-container",
+      scrub: true,
+      start: "top bottom",
+      end: "bottom top",
+    },
+  });
 
-//   gsap.from(".clouds", {
-//     y: "25%",
-//     scrollTrigger: {
-//       trigger: ".ground-container",
-//       scrub: true,
-//       start: "top bottom",
-//       end: "bottom top",
-//     },
-//   });
+  gsap.from(".clouds", {
+    y: "25%",
+    scrollTrigger: {
+      trigger: ".ground-container",
+      scrub: true,
+      start: "top bottom",
+      end: "bottom top",
+    },
+  });
 
-//   gsap.to("#line-length", {
-//     drawSVG: 100,
-//     scrollTrigger: {
-//       trigger: ".length",
-//       scrub: true,
-//       start: "top bottom",
-//       end: "top top",
-//     },
-//   });
+  gsap.to("#line-length", {
+    drawSVG: 100,
+    scrollTrigger: {
+      trigger: ".length",
+      scrub: true,
+      start: "top bottom",
+      end: "top top",
+    },
+  });
 
-//   gsap.to("#line-wingspan", {
-//     drawSVG: 100,
-//     scrollTrigger: {
-//       trigger: ".wingspan",
-//       scrub: true,
-//       start: "top 25%",
-//       end: "bottom 50%",
-//     },
-//   });
+  gsap.to("#line-wingspan", {
+    drawSVG: 100,
+    scrollTrigger: {
+      trigger: ".wingspan",
+      scrub: true,
+      start: "top 25%",
+      end: "bottom 50%",
+    },
+  });
 
-//   gsap.to("#circle-phalange", {
-//     drawSVG: 100,
-//     scrollTrigger: {
-//       trigger: ".phalange",
-//       scrub: true,
-//       start: "top 50%",
-//       end: "bottom 100%",
-//     },
-//   });
+  gsap.to("#circle-phalange", {
+    drawSVG: 100,
+    scrollTrigger: {
+      trigger: ".phalange",
+      scrub: true,
+      start: "top 50%",
+      end: "bottom 100%",
+    },
+  });
 
-//   gsap.to("#line-length", {
-//     opacity: 0,
-//     drawSVG: 0,
-//     scrollTrigger: {
-//       trigger: ".length",
-//       scrub: true,
-//       start: "top top",
-//       end: "bottom top",
-//     },
-//   });
+  gsap.to("#line-length", {
+    opacity: 0,
+    drawSVG: 0,
+    scrollTrigger: {
+      trigger: ".length",
+      scrub: true,
+      start: "top top",
+      end: "bottom top",
+    },
+  });
 
-//   gsap.to("#line-wingspan", {
-//     opacity: 0,
-//     drawSVG: 0,
-//     scrollTrigger: {
-//       trigger: ".wingspan",
-//       scrub: true,
-//       start: "top top",
-//       end: "bottom top",
-//     },
-//   });
+  gsap.to("#line-wingspan", {
+    opacity: 0,
+    drawSVG: 0,
+    scrollTrigger: {
+      trigger: ".wingspan",
+      scrub: true,
+      start: "top top",
+      end: "bottom top",
+    },
+  });
 
-//   gsap.to("#circle-phalange", {
-//     opacity: 0,
-//     drawSVG: 0,
-//     scrollTrigger: {
-//       trigger: ".phalange",
-//       scrub: true,
-//       start: "top top",
-//       end: "bottom top",
-//     },
-//   });
+  gsap.to("#circle-phalange", {
+    opacity: 0,
+    drawSVG: 0,
+    scrollTrigger: {
+      trigger: ".phalange",
+      scrub: true,
+      start: "top top",
+      end: "bottom top",
+    },
+  });
 
-//   let tl = new gsap.timeline({
-//     onUpdate: scene.renderer,
-//     scrollTrigger: {
-//       trigger: ".content",
-//       scrub: true,
-//       start: "top top",
-//       end: "bottom bottom",
-//     },
-//     defaults: { duration: sectionDuration, ease: "power2.inOut" },
-//   });
+  let tl = new gsap.timeline({
+    onUpdate: scene.renderer,
+    scrollTrigger: {
+      trigger: ".content",
+      scrub: true,
+      start: "top top",
+      end: "bottom bottom",
+    },
+    defaults: { duration: sectionDuration, ease: "power2.inOut" },
+  });
 
-//   let delay = 0;
+  let delay = 0;
 
-//   tl.to(".scroll-cta", { duration: 0.25, opacity: 0 }, delay);
-//   tl.to(plane.position, { x: -10, ease: "power1.in" }, delay);
+  tl.to(".scroll-cta", { duration: 0.25, opacity: 0 }, delay);
+  tl.to(plane.position, { x: -10, ease: "power1.in" }, delay);
 
-//   delay += sectionDuration;
+  delay += sectionDuration;
 
-//   tl.to(
-//     plane.rotation,
-//     { x: tau * 0.25, y: 0, z: -tau * 0.05, ease: "power1.inOut" },
-//     delay
-//   );
-//   tl.to(plane.position, { x: -40, y: 0, z: -60, ease: "power1.inOut" }, delay);
+  tl.to(
+    plane.rotation,
+    { x: tau * 0.25, y: 0, z: -tau * 0.05, ease: "power1.inOut" },
+    delay
+  );
+  tl.to(plane.position, { x: -40, y: 0, z: -60, ease: "power1.inOut" }, delay);
 
-//   delay += sectionDuration;
+  delay += sectionDuration;
 
-//   tl.to(
-//     plane.rotation,
-//     { x: tau * 0.25, y: 0, z: tau * 0.05, ease: "power3.inOut" },
-//     delay
-//   );
-//   tl.to(plane.position, { x: 40, y: 0, z: -60, ease: "power2.inOut" }, delay);
+  tl.to(
+    plane.rotation,
+    { x: tau * 0.25, y: 0, z: tau * 0.05, ease: "power3.inOut" },
+    delay
+  );
+  tl.to(plane.position, { x: 40, y: 0, z: -60, ease: "power2.inOut" }, delay);
 
-//   delay += sectionDuration;
+  delay += sectionDuration;
 
-//   tl.to(
-//     plane.rotation,
-//     { x: tau * 0.2, y: 0, z: -tau * 0.1, ease: "power3.inOut" },
-//     delay
-//   );
-//   tl.to(plane.position, { x: -40, y: 0, z: -30, ease: "power2.inOut" }, delay);
+  tl.to(
+    plane.rotation,
+    { x: tau * 0.2, y: 0, z: -tau * 0.1, ease: "power3.inOut" },
+    delay
+  );
+  tl.to(plane.position, { x: -40, y: 0, z: -30, ease: "power2.inOut" }, delay);
 
-//   delay += sectionDuration;
+  delay += sectionDuration;
 
-//   tl.to(plane.rotation, { x: 0, z: 0, y: tau * 0.25 }, delay);
-//   tl.to(plane.position, { x: 0, y: -10, z: 50 }, delay);
+  tl.to(plane.rotation, { x: 0, z: 0, y: tau * 0.25 }, delay);
+  tl.to(plane.position, { x: 0, y: -10, z: 50 }, delay);
 
-//   delay += sectionDuration;
-//   delay += sectionDuration;
+  delay += sectionDuration;
+  delay += sectionDuration;
 
-//   tl.to(
-//     plane.rotation,
-//     { x: tau * 0.25, y: tau * 0.5, z: 0, ease: "power4.inOut" },
-//     delay
-//   );
-//   tl.to(plane.position, { z: 30, ease: "power4.inOut" }, delay);
+  tl.to(
+    plane.rotation,
+    { x: tau * 0.25, y: tau * 0.5, z: 0, ease: "power4.inOut" },
+    delay
+  );
+  tl.to(plane.position, { z: 30, ease: "power4.inOut" }, delay);
 
-//   delay += sectionDuration;
+  delay += sectionDuration;
 
-//   tl.to(
-//     plane.rotation,
-//     { x: tau * 0.25, y: tau * 0.5, z: 0, ease: "power4.inOut" },
-//     delay
-//   );
-//   tl.to(plane.position, { z: 60, x: 30, ease: "power4.inOut" }, delay);
+  tl.to(
+    plane.rotation,
+    { x: tau * 0.25, y: tau * 0.5, z: 0, ease: "power4.inOut" },
+    delay
+  );
+  tl.to(plane.position, { z: 60, x: 30, ease: "power4.inOut" }, delay);
 
-//   delay += sectionDuration;
+  delay += sectionDuration;
 
-//   tl.to(
-//     plane.rotation,
-//     { x: tau * 0.35, y: tau * 0.75, z: tau * 0.6, ease: "power4.inOut" },
-//     delay
-//   );
-//   tl.to(plane.position, { z: 100, x: 20, y: 0, ease: "power4.inOut" }, delay);
+  tl.to(
+    plane.rotation,
+    { x: tau * 0.35, y: tau * 0.75, z: tau * 0.6, ease: "power4.inOut" },
+    delay
+  );
+  tl.to(plane.position, { z: 100, x: 20, y: 0, ease: "power4.inOut" }, delay);
 
-//   delay += sectionDuration;
+  delay += sectionDuration;
 
-//   tl.to(
-//     plane.rotation,
-//     { x: tau * 0.15, y: tau * 0.85, z: -tau * 0, ease: "power1.in" },
-//     delay
-//   );
-//   tl.to(plane.position, { z: -150, x: 0, y: 0, ease: "power1.inOut" }, delay);
+  tl.to(
+    plane.rotation,
+    { x: tau * 0.15, y: tau * 0.85, z: -tau * 0, ease: "power1.in" },
+    delay
+  );
+  tl.to(plane.position, { z: -150, x: 0, y: 0, ease: "power1.inOut" }, delay);
 
-//   delay += sectionDuration;
+  delay += sectionDuration;
 
-//   tl.to(
-//     plane.rotation,
-//     {
-//       duration: sectionDuration,
-//       x: -tau * 0.05,
-//       y: tau,
-//       z: -tau * 0.1,
-//       ease: "none",
-//     },
-//     delay
-//   );
-//   tl.to(
-//     plane.position,
-//     { duration: sectionDuration, x: 0, y: 30, z: 320, ease: "power1.in" },
-//     delay
-//   );
+  tl.to(
+    plane.rotation,
+    {
+      duration: sectionDuration,
+      x: -tau * 0.05,
+      y: tau,
+      z: -tau * 0.1,
+      ease: "none",
+    },
+    delay
+  );
+  tl.to(
+    plane.position,
+    { duration: sectionDuration, x: 0, y: 30, z: 320, ease: "power1.in" },
+    delay
+  );
 
-//   tl.to(
-//     scene.light.position,
-//     { duration: sectionDuration, x: 0, y: 0, z: 0 },
-//     delay
-//   );
-// }
+  tl.to(
+    scene.light.position,
+    { duration: sectionDuration, x: 0, y: 0, z: 0 },
+    delay
+  );
+}
 
 // loadModel();
-
-const scene = new THREE.Scene();
-scene.add(new THREE.AxesHelper(5));
-
-const light1 = new THREE.PointLight(0xffffff, 100);
-light1.position.set(2.5, 2.5, 2.5);
-scene.add(light1);
-
-const light2 = new THREE.PointLight(0xffffff, 100);
-light2.position.set(-2.5, 2.5, 2.5);
-scene.add(light2);
-
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.set(0.8, 1, 1.0);
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.target.set(0, 1, 0);
 
 // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
 
@@ -498,92 +586,6 @@ controls.target.set(0, 1, 0);
 // );
 // Instantiate a loader
 
-let mixer: THREE.AnimationMixer;
-let modelReady = false;
-const animationActions: THREE.AnimationAction[] = [];
-let activeAction: THREE.AnimationAction;
-let lastAction: THREE.AnimationAction;
-const gltfLoader = new GLTFLoader();
-
-gltfLoader.load(
-  "./src/plane/scene.gltf",
-  (gltf) => {
-    // gltf.scene.scale.set(.01, .01, .01)
-
-    mixer = new THREE.AnimationMixer(gltf.scene);
-
-    const animationAction = mixer.clipAction((gltf as any).animations[0]);
-    animationActions.push(animationAction);
-    animationsFolder.add(animations, "default");
-    activeAction = animationActions[0];
-
-    scene.add(gltf.scene);
-
-    //add an animation from another file
-    gltfLoader.load(
-      "./src/plane/scene.gltf",
-      (gltf) => {
-        console.log("loaded 1");
-        const animationAction = mixer.clipAction((gltf as any).animations[0]);
-        animationActions.push(animationAction);
-        animationsFolder.add(animations, "1");
-
-        //add an animation from another file
-        gltfLoader.load(
-          "./src/plane/scene.gltf",
-          (gltf) => {
-            console.log("loaded 2");
-            const animationAction = mixer.clipAction(
-              (gltf as any).animations[0]
-            );
-            animationActions.push(animationAction);
-            animationsFolder.add(animations, "2");
-
-            //add an animation from another file
-            gltfLoader.load(
-              "./src/plane/scene.gltf",
-              (gltf) => {
-                console.log("loaded 3");
-                (gltf as any).animations[0].tracks.shift(); //delete the specific track that moves the object forward while running
-                const animationAction = mixer.clipAction(
-                  (gltf as any).animations[0]
-                );
-                animationActions.push(animationAction);
-                animationsFolder.add(animations, "3");
-
-                modelReady = true;
-              },
-              (xhr) => {
-                console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-              },
-              (error) => {
-                console.log(error);
-              }
-            );
-          },
-          (xhr) => {
-            console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-          },
-          (error) => {
-            console.log(error);
-          }
-        );
-      },
-      (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  },
-  (xhr) => {
-    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-  },
-  (error) => {
-    console.log(error);
-  }
-);
 // Optional: Provide a DRACOLoader instance to decode compressed mesh data
 // const dracoLoader = new DRACOLoader();
 // dracoLoader.setDecoderPath("/examples/jsm/libs/draco/");
